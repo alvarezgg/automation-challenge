f0238ee0419235e847afeac5f3f2b7aa1c78c021
Gonzalo Alvarez <alvarez.gg@gmail.com>
2023-05-31 00:48:22 -0300

    Agrega selenium manager + soporte browsers

--- src/test/java/scripts/BaseTest.java
+++ src/test/java/scripts/BaseTest.java
@@ -5,12 +5,15 @@ import java.util.Properties;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.safari.SafariDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import utils.Config;
 import utils.EvidenceHelper;
 
@@ -41,7 +44,10 @@ public abstract class BaseTest {
 				return this.getChromeDriver();
 			case "firefox":
 				return this.getGeckoDriver();
-			//	TODO agregar metodos safari, edge
+			case "edge":
+				return this.getEdgeDriver();
+			case "safari":
+				return this.getSafariDriver();
 			default:
 				break;
 		}
@@ -51,20 +57,22 @@ public abstract class BaseTest {
 	private WebDriver getGeckoDriver() {
 		FirefoxOptions options = new FirefoxOptions();
 //		TODO agregar opciones / levantar de config
-		
+		WebDriverManager.firefoxdriver().setup();
 		return new FirefoxDriver(options);
 	}
 
 	private WebDriver getChromeDriver() {
 		ChromeOptions options = new ChromeOptions();
 //		TODO agregar opciones / levantar de config
-		
-		String chromedriverPath="chromedriver";
-		if(System.getProperty("os.name").toLowerCase().contains("win")) {
-			chromedriverPath ="chromedriver.exe";			
-		}
-		System.setProperty("webdriver.chrome.driver",chromedriverPath);
-		
+		WebDriverManager.chromedriver().setup();
 		return new ChromeDriver(options);
+	}
+	private WebDriver getEdgeDriver() {
+		WebDriverManager.edgedriver().setup();
+		return new EdgeDriver();
+	}
+	private WebDriver getSafariDriver() {
+		WebDriverManager.safaridriver().setup();
+		return new SafariDriver();
 	}
 }
